80ec7b8f0b1564dc08d1b304775ba69ef0ab7ace
James Quigley <james.b.quigley93@gmail.com>
2018-01-11 15:34:31 -0700

    added airbnb eslint config (hopefully)

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -14,6 +14,24 @@ module.exports = {
     "plugins": [
         "react"
     ],
+    "settings" : {
+        'react' : {
+          pragma : 'React',
+          version : '15.0',
+        },
+        'import/resolver' : {
+          webpack : {
+            config : path.join(__dirname, 'hack/config/webpack.config.dev.js'),
+          },
+        },
+        'import/ignore' : [
+          /node_modules/,
+          /\.(scss|css)$/,
+        ],
+      },
+
+    "extends": "airbnb",
+    
     "rules": {
         "indent": [
             "error",
